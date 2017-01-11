9ac5c3170ab6c0d6e7cff6d469891f1f083bef68
Jeremy Thomerson <jeremy@thomersonfamily.com>
2017-01-11 09:35:40 -0500

    Update long description of language counts

--- src/lib/formatter.js
+++ src/lib/formatter.js
@@ -1,6 +1,6 @@
 var _ = require('underscore'),
     SHORT = _.template('Languages: jw.org: <%= jworg.total %>, <%= jworg.hasWebContent %> L1+, <%= jworg.isSign %> SLs, <%= jworg.isRTL %> RTL, <%= jworg.isSignWithWeb %> L1+ SLs; jwb: <%= jwb.web.total %>, <%= jwb.web.isSign %> SLs, <%= jwb.appletv.total %> AppleTV, <%= jwb.roku.total %> Roku'),
-    LONG = _.template('jw.org has <%= jworg.total %> languages with downloadable content, <%= jworg.hasWebContent %> of which have part of the actual site in their language. This includes <%= jworg.isSign %> sign languages with downloadable content, <%= jworg.isSignWithWeb %> of which have part of the actual site in their language. <%= jworg.isRTL %> of these languages are right-to-left.\n\nJW Broadcasting is available in <%= jwb.web.total %> languages, all of which have the monthly program. This includes <%= jwb.web.isSign %> sign languages.');
+    LONG = _.template('jw.org has <%= jworg.total %> languages with downloadable content, <%= jworg.hasWebContent %> of which have part of the actual site in their language. This includes <%= jworg.isSign %> sign languages with downloadable content, <%= jworg.isSignWithWeb %> of which have part of the actual site in their language. <%= jworg.isRTL %> of these languages are right-to-left.\n\nJW Broadcasting is available in <%= jwb.web.total %> languages. This includes <%= jwb.web.isSign %> sign languages.');
 
 module.exports = {
 
